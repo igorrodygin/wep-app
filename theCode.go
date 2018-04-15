e3024e3bcfb4eb33c12bd342ab491f3d25f19547
Igor Rodygin <igor.rodygin@gmail.com>
2018-04-15 13:59:38 +0300

    First Commit

---
+++ theCode.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
